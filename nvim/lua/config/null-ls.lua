a9416c363c97ef68c82353725f7fe5f713f71882
Daniel Demara <daniel@dcubed.dev>
2022-11-11 20:18:35 -0600

    Fix PHP formatting/diagnostics

--- nvim/lua/config/null-ls.lua
+++ nvim/lua/config/null-ls.lua
@@ -1,19 +1,33 @@
 local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+local phpcs_in_root = require("null-ls.utils").make_conditional_utils().root_has_file({ ".phpcs.xml" })
+local phpcs_extra_args = function(params)
+	if phpcs_in_root then
+		return {}
+	end
+
+	return {
+		"--standard=src/.phpcs.xml",
+	}
+end
+
 null_ls.setup({
 	sources = {
+		-- Code Actions
 		null_ls.builtins.code_actions.gitsigns,
-		null_ls.builtins.diagnostics.eslint_d.with({
-			disabled_filetypes = { "vue" }, -- Let Volar LSP Take Over
+
+		-- PHP
+		null_ls.builtins.formatting.phpcbf.with({
+			prefer_local = "vendor/bin",
+			extra_args = phpcs_extra_args,
 		}),
-		null_ls.builtins.formatting.prettierd,
-		null_ls.builtins.formatting.google_java_format.with({
-			extra_args = { "--aosp" }, -- Use AOSP style to use 4 space indentation instead of 2
-		}),
-		null_ls.builtins.formatting.goimports,
-		null_ls.builtins.formatting.gofumpt,
-		null_ls.builtins.formatting.black,
+		-- null_ls.builtins.diagnostics.phpcs.with({
+		--     prefer_local = "vendor/bin",
+		--     extra_args = phpcs_extra_args,
+		-- }),
+
+		-- Python
 		null_ls.builtins.diagnostics.flake8.with({
 			prefer_local = "venv/bin",
 			condition = function(utils)
@@ -26,12 +40,24 @@ null_ls.setup({
 				return utils.root_has_file({ ".pylintrc" })
 			end,
 		}),
-		null_ls.builtins.diagnostics.phpcs.with({
-			prefer_local = "vendor/bin",
+		null_ls.builtins.formatting.black,
+
+		-- JS
+		null_ls.builtins.diagnostics.eslint_d.with({
+			disabled_filetypes = { "vue" }, -- Let Volar LSP Take Over
 		}),
-		null_ls.builtins.formatting.phpcbf.with({
-			prefer_local = "vendor/bin",
+		null_ls.builtins.formatting.prettierd,
+
+		-- Java
+		null_ls.builtins.formatting.google_java_format.with({
+			extra_args = { "--aosp" }, -- Use AOSP style to use 4 space indentation instead of 2
 		}),
+
+		-- Go
+		null_ls.builtins.formatting.goimports,
+		null_ls.builtins.formatting.gofumpt,
+
+		-- Lua
 		null_ls.builtins.formatting.stylua,
 	},
 	on_attach = function(client, bufnr)
