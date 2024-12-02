e6f872a8488d4db9cec7f8b624f5cf53435890a3
Daniel Demara <daniel@dcubed.dev>
2024-12-02 10:44:52 -0600

    Add nvim auto format terragrunt hcl

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -2,14 +2,27 @@ return {
 	-- Provides formatting in cases where an LSP doesn't support it or a different style is needed
 	"stevearc/conform.nvim",
 	opts = {
+		notify_on_error = false,
+		format_on_save = function(bufnr)
+			-- Disable "format_on_save lsp_fallback" for languages that don't
+			-- have a well standardized coding style. You can add additional
+			-- languages here or re-enable it for the disabled ones.
+			local disable_filetypes = { c = true, cpp = true }
+			local lsp_format_opt
+			if disable_filetypes[vim.bo[bufnr].filetype] then
+				lsp_format_opt = "never"
+			else
+				lsp_format_opt = "fallback"
+			end
+			return {
+				timeout_ms = 500,
+				lsp_format = lsp_format_opt,
+			}
+		end,
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "black" },
-		},
-		format_on_save = {
-			-- These options will be passed to conform.format()
-			timeout_ms = 500,
-			lsp_format = "fallback",
+			hcl = { "terragrunt_hclfmt" },
 		},
 	},
 }
